02265b9b6ec5053f54b7886df430777865f7d7ba
presleyoreed3 <presleyoreed3@gmail.com>
2022-01-21 12:03:03 -0800

    rendering single h1

---
+++ frontend/startoff.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import  Root  from './components/root'
+
+document.addEventListener("DOMContentLoaded", () => {
+	const root = document.getElementById('root');
+	// const store = configureStore()
+	//Testing
+
+	//Testing
+
+	ReactDOM.render(<Root />, root)
+})
