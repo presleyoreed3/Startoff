d86f94cb02b43c04245e0d74d4284820b40dceee
presleyoreed3 <presleyoreed3@gmail.com>
2022-01-25 14:15:54 -0800

    photo alignment

--- frontend/components/projects/project_index_item.jsx
+++ frontend/components/projects/project_index_item.jsx
@@ -6,9 +6,13 @@ class ProjectsIndexItem extends React.Component {
 	render(){
 		return(
 			<div id="project-div">
-				<h2>{this.props.project.project_name}</h2>
-				<img src={this.props.project.photoUrl}></img>
-				<p>{this.props.project.description}</p>
+				<div id="top-image">
+					<img src={this.props.project.photoUrl}></img>
+				</div>
+				<div id="description">
+					<h2>{this.props.project.project_name}</h2>
+					<p>{this.props.project.description}</p>
+				</div>
 			</div>
 		)
 	}
