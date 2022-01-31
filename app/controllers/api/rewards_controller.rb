55e0b85d43fad5d482b218bc760db79edbd3ad2c
presleyoreed3 <presleyoreed3@gmail.com>
2022-01-31 09:32:36 -0800

    controller for rewards creation

--- app/controllers/api/rewards_controller.rb
+++ app/controllers/api/rewards_controller.rb
@@ -1,3 +1,18 @@
 class Api::RewardsController < ApplicationController
 
+	def create
+		@reward = Reward.new(reward_params)
+		if @reward.save
+			redirect_to api_project
+		else
+			render json: @reward.errors.full_messages, status: 422
+		end
+	end
+
+	private
+
+	def reward_params
+		params.require(:reward).permit(:tier_name, :reward, :reward_description, :price, :project_id,)
+	end
+
 end
