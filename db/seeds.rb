22fc094ab527ddc9ed4f7ed98dd5f11cd7c1135b
presleyoreed3 <presleyoreed3@gmail.com>
2022-02-03 14:00:18 -0800

    fixed issue with seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destory_all
+Project.destroy_all
+Reward.destory_all
+
 require "open-uri"
 
 users = User.create([
