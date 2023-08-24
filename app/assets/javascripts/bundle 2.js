14b766efb6126b271df69b4c9ad3c6f6168ec2aa
presleyoreed3 <presleyoreed3@gmail.com>
2023-08-24 10:01:51 -0700

    rails and ruby updates

---
+++ app/assets/javascripts/bundle 2.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./frontend/startoff.jsx":
+/*!*******************************!*\
+  !*** ./frontend/startoff.jsx ***!
+  \*******************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: Cannot find module './ast-types/generated'\nRequire stack:\n- /Users/presleythe3/Desktop/App-Academy/Startoff/node_modules/@babel/types/lib/index.js\n- /Users/presleythe3/Desktop/App-Academy/Startoff/node_modules/@babel/core/lib/transformation/file/file.js\n- /Users/presleythe3/Desktop/App-Academy/Startoff/node_modules/@babel/core/lib/index.js\n- /Users/presleythe3/Desktop/App-Academy/Startoff/node_modules/babel-loader/lib/index.js\n- /Users/presleythe3/Desktop/App-Academy/Startoff/node_modules/loader-runner/lib/loadLoader.js\n- /Users/presleythe3/Desktop/App-Academy/Startoff/node_modules/loader-runner/lib/LoaderRunner.js\n- /Users/presleythe3/Desktop/App-Academy/Startoff/node_modules/webpack/lib/NormalModuleFactory.js\n- /Users/presleythe3/Desktop/App-Academy/Startoff/node_modules/webpack/lib/Compiler.js\n- /Users/presleythe3/Desktop/App-Academy/Startoff/node_modules/webpack/lib/webpack.js\n- /Users/presleythe3/Desktop/App-Academy/Startoff/node_modules/webpack/lib/index.js\n- /Users/presleythe3/Desktop/App-Academy/Startoff/node_modules/webpack-cli/lib/webpack-cli.js\n- /Users/presleythe3/Desktop/App-Academy/Startoff/node_modules/webpack-cli/lib/bootstrap.js\n- /Users/presleythe3/Desktop/App-Academy/Startoff/node_modules/webpack-cli/bin/cli.js\n- /Users/presleythe3/.nvm/versions/node/v14.18.2/lib/node_modules/webpack-cli/node_modules/import-local/index.js\n- /Users/presleythe3/.nvm/versions/node/v14.18.2/lib/node_modules/webpack-cli/bin/cli.js\n- /Users/presleythe3/.nvm/versions/node/v14.18.2/lib/node_modules/webpack/bin/webpack.js\n babel-loader@8 requires Babel 7.x (the package '@babel/core'). If you'd like to use Babel 6.x ('babel-core'), you should install 'babel-loader@7'.\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:902:15)\n    at Function.Module._load (internal/modules/cjs/loader.js:746:27)\n    at Module.require (internal/modules/cjs/loader.js:974:19)\n    at require (internal/modules/cjs/helpers.js:93:18)\n    at Object.<anonymous> (/Users/presleythe3/Desktop/App-Academy/Startoff/node_modules/@babel/types/lib/index.js:629:19)\n    at Module._compile (internal/modules/cjs/loader.js:1085:14)\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:1114:10)\n    at Module.load (internal/modules/cjs/loader.js:950:32)\n    at Function.Module._load (internal/modules/cjs/loader.js:790:12)\n    at Module.require (internal/modules/cjs/loader.js:974:19)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./frontend/startoff.jsx"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
